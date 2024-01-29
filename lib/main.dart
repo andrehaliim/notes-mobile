2328f351386cbb492a042326f2b067b119520a5c
Andre Haliim <andre@infonetglobal.tech>
2024-01-29 15:01:56 +0700

    create basic ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,40 +14,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,70 +33,136 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  bool deleteButton = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
-  @override
+@override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    double mainWidth = MediaQuery.of(context).size.width;
+    double mainHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Container(
+        margin: EdgeInsets.only(top: 40),
+        child: Stack(
+          children: [
+            Positioned(
+              top: 10,
+              right: 0,
+              child: IconButton(
+                icon: Icon(CupertinoIcons.trash_circle_fill, color: Colors.red,),
+                iconSize: 40,
+                onPressed: (){
+                  setState(() {
+                    deleteButton = !deleteButton;
+                  });
+                },
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(15),
+                  child: Text(
+                    'Welcome back,\n{username}',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                    margin: EdgeInsets.only(left: 10, right: 10),
+                    width: mainWidth,
+                    height: mainHeight * 0.83,
+                    child: GridView.builder(
+                        physics: const ScrollPhysics(),
+                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 3.0,
+                          mainAxisSpacing: 3.0,
+                        ),
+                        shrinkWrap: true,
+                        itemCount: 15,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Container(
+                            margin: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.2), // Shadow color
+                                  spreadRadius: 2, // Spread radius of the shadow
+                                  blurRadius: 4,   // Blur radius of the shadow
+                                  offset: Offset(0, 2), // Offset of the shadow
+                                ),
+                              ],
+                            ),
+                            child: Stack(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children:  [
+                                      Text('What is Lorem Ipsum?',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold
+                                      ),
+                                      maxLines: 2,
+                                      overflow: TextOverflow.ellipsis,),
+                                      Text("Lorem Ipsum is simply dummy text of the printing"
+                                          " and typesetting industry. Lorem Ipsum has"
+                                          " been the industry's standard dummy text"
+                                          " ever since the 1500s, when an unknown printer"
+                                          " took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,"
+                                          " but also the leap into electronic typesetting, remaining essentially unchanged."
+                                          " It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,"
+                                          " and more recently with "
+                                          "desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                                      maxLines: 4,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(
+                                        fontSize: 11
+                                      ),),
+                                      Container(
+                                        margin: EdgeInsets.only(top: 15),
+                                        child: Text('26 Jan 2024',
+                                            style: TextStyle(
+                                                fontSize: 11
+                                            )),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                Positioned(
+                                  bottom: 0,
+                                  right: 0,
+                                  child: IconButton(
+                                    icon: Icon(deleteButton == false ? CupertinoIcons.pencil_circle_fill : CupertinoIcons.trash_circle_fill),
+                                    iconSize: 30,
+                                    onPressed: () {
+                                      // Add your button onPressed logic here
+                                    },
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        })),
+              ],
             ),
+            Positioned(
+              bottom: 10,
+              left: 0,
+              right: 0,
+              child: IconButton(
+                icon: Icon(CupertinoIcons.plus_circle_fill, color: Colors.amber,),
+                iconSize: 50,
+                onPressed: null,
+              ),
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
